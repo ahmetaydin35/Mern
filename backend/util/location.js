c55778305616d88a37d3847417ce6ec4c1087bc7
Ahmet Aydin <ahm3t35@gmail.com>
2024-07-05 13:08:59 +0300

    update

--- backend/util/location.js
+++ backend/util/location.js
@@ -6,8 +6,7 @@ const API_KEY = "AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA";
 
 async function getCoordsForAddress(address) {
   // return {
-  //   lat: 40.7484474,
-  //   lng: -73.9871516
+
   const response = await axios.get(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
       address
